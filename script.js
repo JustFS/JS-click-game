ede9aeada79aedf6002fce6bc7f01ec1e24db90a
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-04-13 03:14:29 +0200

    added style

--- script.js
+++ script.js
@@ -3,70 +3,63 @@ const score = document.getElementById('score');
 const days = document.getElementById('days');
 const endScreen = document.getElementById('endScreen');
 
-let loopPlay = false; 
+daysLeft = 60;
+gameOverNumber = 50;
+loopPlay = false;
 
 function start() {
-
-  let getFaster = 6000;
-  let count = 0;
-  let daysRemaining = 10;
+  count = 0;
+  getFaster = 6000;
+  daysRemaining = daysLeft; 
 
   canvas.innerHTML = '';
   score.innerHTML = count;
   days.innerHTML = daysRemaining;
 
   // make sure to not play loop several times
-  loopPlay ? getFaster = 6000 : loop(); 
-  console.log('après formule => ' + getFaster);
-  
+  loopPlay ? '' : game();   
   loopPlay = true;
 
-  function loop() {
-    let randomTimeout = Math.round(Math.random() * getFaster);
+  function game() {
+    let randomTime = Math.round(Math.random() * getFaster);
     getFaster > 700 ? getFaster = (getFaster * 0.90) : '';
   
     setTimeout(() => {
       if (daysRemaining === 0){
         youWin();
-      } else if (canvas.childElementCount < 20){
+      } else if (canvas.childElementCount < gameOverNumber){
         virusPop();
-        loop();
+        game();
       } else {
         gameOver();
       }
-    }, randomTimeout);  
-    console.log(getFaster);
-
-    
+    }, randomTime);  
   };
 
-canvas.addEventListener('click', () => {
-  daysRemaining--;
-  days.innerHTML = daysRemaining;
-});
+  const gameOver = () => {
+    endScreen.innerHTML = `<div class="gameOver">Game over <br/>score : ${count} </div>`;
+    endScreen.style.visibility = 'visible';
+    endScreen.style.opacity = '1';
+    loopPlay = false;
+  };
 
-const gameOver = () => {
-  endScreen.innerHTML = `<div class="gameOver">Game over <br/>score : ${count} </div>`;
-  endScreen.style.visibility = 'visible';
-  endScreen.style.opacity = '1';
-  loopPlay = false;
+  const youWin = () => {
+    let accuracy = Math.round(count / daysLeft * 100);
+    endScreen.innerHTML = `<div class="youWin">Well done ! You overcome this mother fucker ! <br/><span>Accuarcy: ${accuracy} %</span></div>`;
+    endScreen.style.visibility = 'visible';
+    endScreen.style.opacity = '1';
+    loopPlay = false; 
+  };
 };
-
-const youWin = () => {
-  const accuracy = Math.round(count / 10 * 100);
-  endScreen.innerHTML = `<div class="youWin">Well done ! You overcome this mother fucker ! <br/>Accuarcy: ${accuracy} %</div>`;
-  endScreen.style.visibility = 'visible';
-  endScreen.style.opacity = '1';
-  loopPlay = false; 
-}
 
 // create random element
 function virusPop() {
-  const virus = new Image();
+  let virus = new Image();
 
   virus.src = "./media/basic-pics/pngwave.png"
 
   virus.classList.add('virus');
+  virus.classList.add('virus-bis');
   virus.style.top = Math.random() * 500 + 'px';
   virus.style.left = Math.random() * 500 + 'px';
 
@@ -84,19 +77,29 @@ function virusPop() {
   canvas.appendChild(virus);
 };
 
-  // remove element clicked
-  document.addEventListener("click", function(e){
-    const targetElement = e.target || e.srcElement;
+// countdown on click
+canvas.addEventListener('click', () => {
+  if (daysRemaining > 0) {
+    daysRemaining--;
+    days.innerHTML = daysRemaining;
+  }
+});
 
-    if (targetElement.classList.contains('virus')) {
-      targetElement.remove();
-      count++;
-      score.innerHTML = count;
-    };
-  });
-};
+// remove element clicked
+document.addEventListener("click", function(e){
+  let targetElement = e.target || e.srcElement;
 
+  if (targetElement.classList.contains('virus')) {
+    targetElement.remove();
+    count++;
+    score.innerHTML = count;
+  };
+});
+
+// hide and screen on click
 endScreen.addEventListener('click', () => {
   start();
-  endScreen.style.visibility = 'hidden';
+  setTimeout(() => {
+    endScreen.style.visibility = 'hidden';
+  }, 1000);
 });
